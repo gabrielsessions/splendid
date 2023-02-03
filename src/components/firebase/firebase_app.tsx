5c6e1677a233becedc92407e384d32e160ffa2b7
Gabriel Sessions <gsess7732@gmail.com>
2023-02-03 01:42:27 -0500

    Created a basic navbar
    Currently working on the sign-in process

---
+++ src/components/firebase/firebase_app.tsx
@@ -0,0 +1,10 @@
+/*
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import  { useCollectionData } from 'react-firebase-hooks/firestore'
+import { getFirestore, doc, getDoc, setDoc, collection, query, orderBy, limit, Timestamp } from 'firebase/firestore';
+import { initializeApp } from '@firebase/app';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+*/
